c9e923f9ee755bebf82edf90235e7781bec84b44
rrafush <design.rafaelamartins@gmail.com>
2024-10-26 12:24:55 -0300

    setup locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,10 +1,35 @@
 import 'package:get_it/get_it.dart';
 import 'package:simple_to_do_app/to_do_list.dart/data/repository.dart';
+import 'package:simple_to_do_app/to_do_list.dart/domain/usecases/create_to_do_usecase.dart';
+import 'package:simple_to_do_app/to_do_list.dart/domain/usecases/delete_to_do_usecase.dart';
+import 'package:simple_to_do_app/to_do_list.dart/domain/usecases/get_to_dos_usecase.dart';
+import 'package:simple_to_do_app/to_do_list.dart/domain/usecases/update_to_do_usecase.dart';
 import 'package:simple_to_do_app/to_do_list.dart/utils/adapters/shared_preferences_adapter.dart';
 
 final locator = GetIt.instance;
 
 void setupLocator() {
-  locator.registerLazySingleton<SharedPreferencesAdapter>(() => SharedPreferencesAdapterImpl());
+  locator.registerLazySingleton<SharedPreferencesAdapter>(
+      () => SharedPreferencesAdapterImpl());
   locator.registerLazySingleton<Repository>(() => RepositoryImpl());
+  locator.registerLazySingleton<CreateToDoUsecase>(
+    () => CreateToDoUsecaseImpl(
+      locator<Repository>(),
+    ),
+  );
+  locator.registerLazySingleton<DeleteToDoUsecase>(
+    () => DeleteToDoUsecaseImpl(
+      locator<Repository>(),
+    ),
+  );
+  locator.registerLazySingleton<GetToDosUsecase>(
+    () => GetToDosUsecaseImpl(
+      locator<Repository>(),
+    ),
+  );
+  locator.registerLazySingleton<UpdateToDoUsecase>(
+    () => UpdateToDoUsecaseImpl(
+      locator<Repository>(),
+    ),
+  );
 }
